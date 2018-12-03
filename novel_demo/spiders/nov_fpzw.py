f3342905d84bb8be03095f14aa68dee5bac0b38c
wzw5566 <wzw33874@qq.com>
2018-12-04 06:35:59 +0800

    抓取小说标题

--- novel_demo/spiders/nov_fpzw.py
+++ novel_demo/spiders/nov_fpzw.py
@@ -3,7 +3,15 @@ import scrapy
 from scrapy.http import Request
 from novel_demo.items import NovelDemoItem
 
+#定义抓取的整体页数
+def start_requests(self):
+    for i in range(1, 51):
+        url = "https://www.fpzw.com/" + str(i)
+        yield scrapy.Request(url, callback=self.pare)
+
+
 class NovFpzwSpider(scrapy.Spider):
+
     name = 'nov_fpzw'
     allowed_domains = ['fpzw.com']
     start_urls = ['https://www.fpzw.com/1']
@@ -14,10 +22,11 @@ class NovFpzwSpider(scrapy.Spider):
         item = NovelDemoItem()
         item["nov_name"] = response.xpath("//div[@id='title']/h2/a/text()").extract()
         #item["nov_chapter_link"] = response.xpath("/dl[@class='book']/dd[5:]/a/@href").extract()
+        detail_url = response.xpath("//div[@id='opt']//li[@id='bt_1']/a/@href").extract()
+
         yield item
 
-        for i in range(2, 100):
+    def parse_detail(self, response):
 
-            url = "https://www.fpzw.com/" +str(i)
 
-            yield Request(url, callback=self.parse)
+        pass
